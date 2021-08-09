689b82fc013da83843faba5843b7d9b6a09b5587
nakamura <toufu0424@ezweb.ne.jp>
2021-08-09 15:38:46 +0900

    いったんコミットします

--- TestSceneNakamura.h
+++ TestSceneNakamura.h
@@ -1,436 +1,70 @@
-#pragma once
-#include "GameScene_easy.h"
-#include "Result.h"
-#include "Mark.h"
-#include "Target.h"
-#include "Player.h"
-#include "ObstructManager.h"
-#include "Hitchecker.h"
-#include "UI.h"
-#include "Camera.h"
+ï»¿#pragma once
 
+#include "SceneBase.h"
 #include "DxLib.h"
-#include "Effect.h"
 
-static int enemyNum = 10;					//	ƒGƒlƒ~[‚Ì”
-static int GIRL_Y = 0;						//	’†‰Ø—q‚Ì‰ŠúYÀ•W
-static int LADY_Y = 0;						//	’†‰Ø—«‚ÌYÀ•W
-static int GIRL_MIN_Y = -80;				//	’†‰Ø—q‚ÌÅ¬YÀ•W
-static int COUNTDOWN = 7;					//	ƒJƒEƒ“ƒgƒ_ƒEƒ“‚Ì•b”i+2j
-
-////’†‰Ø­—‚Ì‘¬“x
-//static float girlSpeed = 80.0f;
-
-// ƒ^[ƒQƒbƒg‚ª”ò‚ñ‚Å‚­‚éŠÔŠu (•b’PˆÊ)
-const int TARGET_SHOT_INTERVAL = 2;
-// ƒ^[ƒQƒbƒg‚Ì‘¬“x‚ğ‰Šú‰»
-static float targetSpeed = 200.0f;
-
-//	ƒXƒNƒŠ[ƒ“‚ÌƒTƒCƒY
-const int SCREEN_SIZE_W = 1920;
-const int SCREEN_SIZE_H = 1080;
-
-//	ƒtƒF[ƒhƒCƒ“‚Ì‘¬“x
-const int FADE_IN_SPEED = 3;
-//	ƒtƒF[ƒhƒAƒEƒg‚Ì‘¬“x
-const int FADE_OUT_SPEED = 3;
-
-// ‰¹—Ê’²®
-const int GONG_VOLUME_PAL = 30;
-const int DOOR_VOLUME_PAL = 40;
-
-TestSceneNakamura::TestSceneNakamura()
-	:m_player(nullptr)
-	, m_camera(nullptr)
-	, m_mark(nullptr)
-	, m_effect(nullptr)
-	, m_targetCount(0)
-	, m_startTime(0)
-	, m_iceThrowFlag(false)
-	, m_iceHitFlagBuffer(false)
-	, m_girl_Y(GIRL_Y)
-	, m_lady_Y(LADY_Y)
-	, m_girlUpFlag(false)
-	, m_fadeInFinishFlag(false)
-	, m_fadeOutFlag(false)
-	, m_fadeOutFinishFlag(false)
-	, m_girl_hitReactionFlag(false)
-	, m_girl_missReactionFlag(false)
-	, m_girl_ReactionFlag(false)
-	//@Šm”F—p
-	, m_hitCount(0)
-	, m_hitFlag(false)
+class TestSceneNakamura :public SceneBase
 {
-	// Ÿ‚ÌƒV[ƒ“‚ÖˆÚs‚·‚é‚©‚Ç‚¤‚©
-	m_finishFlag = FALSE;
-	// ƒGƒlƒ~[EƒXƒRƒAUI‰Šú‰»
-	for (int i = 0; i < enemyNum; i++)
+public:
+
+	enum class GAME_SCENE_STATE
 	{
-		m_target[i] = nullptr;
-		m_score_ui[i] = nullptr;
-		m_hit_ui[i] = nullptr;
-	}
-	m_target[enemyNum] = nullptr;
+		COUNTDOWN,
+		GAME
+	};
 
-	// ŠJn‚Ìƒ^ƒCƒ€‚ğæ“¾
-	m_startTime = GetNowCount() / 1000;
-	// ƒXƒe[ƒgƒZƒbƒg(ƒJƒEƒ“ƒgƒ_ƒEƒ“‚©‚ç)
-	m_state = GAME_SCENE_STATE::COUNTDOWN;
-}
+	TestSceneNakamura();				//	ã‚³ãƒ³ã‚¹ãƒˆãƒ©ã‚¯ã‚¿
+	~TestSceneNakamura();			//	ãƒ‡ã‚¹ãƒˆãƒ©ã‚¯ã‚¿
 
-TestSceneNakamura::~TestSceneNakamura()
-{
-	delete m_player;	//	ƒvƒŒƒCƒ„[‚Ìƒ|ƒCƒ“ƒ^ƒƒ“ƒo•Ï”‚ğÁ‹
-	delete m_camera;	//	ƒJƒƒ‰‚Ìƒ|ƒCƒ“ƒ^ƒƒ“ƒo•Ï”‚ğÁ‹
-	delete m_mark;		//	ƒ}[ƒN‚Ìƒ|ƒCƒ“ƒ^ƒƒ“ƒo•Ï”‚ğÁ‹
-	//	ƒƒ‚ƒŠ‚Ì‰ğ•úˆ—
-	StopSoundMem(m_finishSoundHandle);
-	DeleteGraph(m_backGraphHandle);
-	DeleteGraph(m_finishGraphHandle);
-	DeleteGraph(m_manualGraphHandle);
-	DeleteGraph(m_girlGraphHandle);
-	DeleteGraph(m_girl_missReaction_GraphHandle);	//  —‚Ìq‚Ì‰æ‘œƒƒ‚ƒŠ‚ğŠJ•ú‚·‚é
-	DeleteGraph(m_girl_hitReaction_GraphHandle);	//  —‚Ìq‚Ì‰æ‘œƒƒ‚ƒŠ‚ğŠJ•ú‚·‚é
-	DeleteGraph(m_ladyGraphHandle);
-	DeleteSoundMem(m_soundHandle);
-	DeleteSoundMem(m_finishSoundHandle);
-	DeleteSoundMem(m_iceSoundHandle);
-	DeleteSoundMem(m_missSoundHandle);
-	DeleteSoundMem(m_hitSoundHandle);
-	DeleteSoundMem(m_doorSoundHandle);
-	for (int i = 0; i < enemyNum; i++)
-	{
-		delete m_target[i];
-		delete m_score_ui[i];
-		delete m_hit_ui[i];
-	}
-	delete m_target[enemyNum];
+	SceneBase* Update(float _deltaTime)override;	//	æ›´æ–°
+	void Draw()override;			//	æç”»
+	void Sound()override;			//	éŸ³æ¥½
+	void Load()override;			//	åˆæœŸåŒ–
+	void DebugKey();
 
-	m_effect->Delete();
-	delete m_effect;
-}
+	int GetTargetCount() { return m_targetCount; }
 
-SceneBase* TestSceneNakamura::Update(float _deltaTime)
-{
-	// ƒfƒoƒbƒOƒrƒ‹ƒh‚Ì‚İƒfƒoƒbƒOŠÖ”‚ğŒÄ‚Ño‚·
-#ifdef _DEBUG
-	DebugKey();
-#endif
+private:
+	class Player* m_player;			//	ãƒ—ãƒ¬ã‚¤ãƒ¤ãƒ¼ã‚¯ãƒ©ã‚¹ã¸ã®ãƒã‚¤ãƒ³ã‚¿ãƒ¡ãƒ³ãƒå¤‰æ•°
+	class Target* m_target[11];			//
+	class Camera* m_camera;			//	ã‚«ãƒ¡ãƒ©ã‚¯ãƒ©ã‚¹ã¸ã®ãƒã‚¤ãƒ³ã‚¿ãƒ¡ãƒ³ãƒå¤‰æ•°
+	class Mark* m_mark;				//	ãƒãƒ¼ã‚¯ã‚¯ãƒ©ã‚¹ã¸ã®ãƒã‚¤ãƒ³ã‚¿ãƒ¡ãƒ³ãƒå¤‰æ•°
+	class UI* m_score_ui[10];		//  UIã‚¯ãƒ©ã‚¹ã¸ã®ãƒã‚¤ãƒ³ã‚¿ãƒ¡ãƒ³ãƒå¤‰æ•°
+	class UI* m_hit_ui[10];			//	ãƒ’ãƒƒãƒˆåˆ¤å®šUIã‚¯ãƒ©ã‚¹ã¸ã®ãƒã‚¤ãƒ³ã‚¿ãƒ¡ãƒ³ãƒå¤‰æ•°
+	class PlayEffect* m_effect;     //  ã‚¨ãƒ•ã‚§ã‚¯ãƒˆãƒ—ãƒ¬ãƒ¼ãƒ¤ãƒ¼
+	GAME_SCENE_STATE m_state;
+	int m_targetCount;				//	ã‚¢ã‚¤ã‚¹ã®é£›ã°ã—ãŸå€‹æ•°
+	int m_startTime;				//	ã‚²ãƒ¼ãƒ ã®é–‹å§‹æ™‚é–“
+	int m_girl_Y;					//	
+	int m_lady_Y;
+	int m_backGraphHandle;			//	èƒŒæ™¯ã®ã‚°ãƒ©ãƒ•ã‚£ãƒƒã‚¯ãƒãƒ³ãƒ‰ãƒ«
+	int m_finishGraphHandle;		//	ã‚²ãƒ¼ãƒ çµ‚äº†æ–‡å­—ã®ã‚°ãƒ©ãƒ•ã‚£ãƒƒã‚¯ãƒãƒ³ãƒ‰ãƒ«
+	int m_soundHandle;				//	ã‚²ãƒ¼ãƒ ç”»é¢ãƒ»ã‚µã‚¦ãƒ³ãƒ‰ãƒãƒ³ãƒ‰ãƒ«
+	int m_finishSoundHandle;		//	ã‚²ãƒ¼ãƒ ç”»é¢ãƒ»çµ‚äº†ã®åŠ¹æœéŸ³ç”¨ã‚µã‚¦ãƒ³ãƒ‰ãƒãƒ³ãƒ‰ãƒ«
+	int m_iceSoundHandle;			//	ã‚¢ã‚¤ã‚¹ã®ç™ºå°„éŸ³ç”¨ã‚µã‚¦ãƒ³ãƒ‰ãƒãƒ³ãƒ‰ãƒ«
+	int m_hitSoundHandle;			//	ã‚¢ã‚¤ã‚¹ã¨çš¿ã®è¡çªéŸ³ç”¨ã‚µã‚¦ãƒ³ãƒ‰ãƒãƒ³ãƒ‰ãƒ«
+	int m_missSoundHandle;			//	ã‚¢ã‚¤ã‚¹ã¨çš¿ãŒè¡çªå¤±æ•—ã—ãŸæ™‚ã®äº¤æ›ç”¨ã‚µã‚¦ãƒ³ãƒ‰ãƒãƒ³ãƒ‰ãƒ«
+	int m_doorSoundHandle;			//	å…¥åº—éŸ³ã®ã‚µã‚¦ãƒ³ãƒ‰ãƒãƒ³ãƒ‰ãƒ«
+	int m_manualGraphHandle;		//	æ“ä½œèª¬æ˜ã®ã‚°ãƒ©ãƒ•ã‚£ãƒƒã‚¯ãƒãƒ³ãƒ‰ãƒ«
+	int m_girlGraphHandle;
+	int m_ladyGraphHandle;
+	int m_girl_missReaction_GraphHandle;	//  ãƒŸã‚¹ã—ãŸæ™‚ã®å¥³ã®å­ã«è¿½åŠ ã™ã‚‹ç”»åƒãƒãƒ³ãƒ‰ãƒ«
+	int m_girl_hitReaction_GraphHandle;		//  æˆåŠŸã—ãŸæ™‚ã®å¥³ã®å­ã«è¿½åŠ ã™ã‚‹ç”»åƒãƒãƒ³ãƒ‰ãƒ«
+	//float m_deltaTime;				//	ãƒ‡ãƒ«ã‚¿ã‚¿ã‚¤ãƒ 
+	bool m_checkKeyFlag;			//	ã‚­ãƒ¼ãŒæŠ¼ã•ã‚ŒãŸã¾ã¾ã‹ã‚’åˆ¤å®šã™ã‚‹ãƒ•ãƒ©ã‚°
+	bool m_finishFlag;				//	ã‚²ãƒ¼ãƒ çµ‚äº†åˆ¤å®šãƒ•ãƒ©ã‚°
+	bool m_iceThrowFlag;			//	ã‚¢ã‚¤ã‚¹å°„å‡ºãƒ•ãƒ©ã‚°
+	bool m_iceHitFlagBuffer;
+	bool m_girlUpFlag;
+	bool m_fadeInFinishFlag;		//	ãƒ•ã‚§ãƒ¼ãƒ‰ã‚¤ãƒ³ã®çµ‚äº†åˆ¤å®šãƒ•ãƒ©ã‚°
+	bool m_fadeOutFlag;				//	ãƒ•ã‚§ãƒ¼ãƒ‰ã‚¢ã‚¦ãƒˆé–‹å§‹ç”¨ã®ãƒ•ãƒ©ã‚°
+	bool m_fadeOutFinishFlag;		//	ãƒ•ã‚§ãƒ¼ãƒ‰ã‚¢ã‚¦ãƒˆã®çµ‚äº†åˆ¤å®šãƒ•ãƒ©ã‚°
 
-	switch (m_state)
-	{
-	case GAME_SCENE_STATE::COUNTDOWN:
-		if ((COUNTDOWN + 1) - (GetNowCount() / 1000 - m_startTime) <= 1)
-		{
-			m_startTime = GetNowCount() / 1000;
-			m_state = GAME_SCENE_STATE::GAME;
-		}
-		break;
-	case GAME_SCENE_STATE::GAME:
-		// Š÷‚ÌXV
-		m_mark->Mark_Update();
+	bool m_girl_hitReactionFlag;	//  å¥³ã®å­ã®hitã—ãŸæ™‚ã®ãƒªã‚¢ã‚¯ã‚·ãƒ§ãƒ³ã‚’ã™ã‚‹ã‹ã©ã†ã‹
+	bool m_girl_missReactionFlag;	//  å¥³ã®å­ã®missã—ãŸæ™‚ã®ã‚¢ã‚¯ã‚·ãƒ§ãƒ³ã‚’ã™ã‚‹ã‹ã©ã†ã‹
+	bool m_girl_ReactionFlag;		//  å¥³ã®å­ã«ãƒªã‚¢ã‚¯ã‚·ãƒ§ãƒ³ã•ã›ã‚‹ã‹ã©ã†ã‹
 
-		if (m_targetCount == 0)
-		{
-			m_target[m_targetCount]->SetSetTime(m_startTime);
-		}
-
-
-		// ƒGƒlƒ~[ËoŠÇ—
-		if (GetNowCount() / 1000 - m_startTime > TARGET_SHOT_INTERVAL)
-		{
-			m_startTime = GetNowCount() / 1000;
-			if (m_target[m_targetCount]->GetIceState() == NO_SHOT)
-			{
-				m_target[m_targetCount]->SetIceState(NOW_SHOT);
-				PlaySoundMem(m_iceSoundHandle, DX_PLAYTYPE_BACK);
-				ChangeVolumeSoundMem(m_volumePal + 20, m_iceSoundHandle);
-			}
-			if (m_target[m_targetCount]->GetIceState() == END_SHOT)
-			{
-				m_girl_ReactionFlag = false;						// —‚Ìq‚ªƒŠƒAƒNƒVƒ‡ƒ“‚µ‚È‚¢‚æ‚¤‚É‚·‚é
-				m_girl_hitReactionFlag = false;
-				m_girl_missReactionFlag = false;
-				m_target[m_targetCount + 1]->SetSetTime(m_startTime);
-				m_targetCount++;
-			}
-		}
-
-		// Œ»İ‚Ì”Ô†‚É‰‚¶‚ÄƒGƒlƒ~[‚ÌXV
-		m_target[m_targetCount]->Update(_deltaTime);
-		m_target[m_targetCount]->SetTargetCount(m_targetCount);
-		m_iceHitFlagBuffer = HitChecker::Check(*m_player, *m_target[m_targetCount]);
-		// x,y,z²‚Ì‚»‚ê‚¼‚ê‚Ìƒ|ƒWƒVƒ‡ƒ“‚ğæ“¾
-		m_target[m_targetCount]->SetSinglePosX();
-		// m_iceHitFlagBuffer‚ªtrue‚É‚È‚Á‚½‚ç
-		if (!m_girl_ReactionFlag && m_target[m_targetCount]->GetIceState() == NOW_SHOT)
-		{
-			if (m_iceHitFlagBuffer)
-			{
-				m_girl_ReactionFlag = true;			// —‚Ìq‚ªƒŠƒAƒNƒVƒ‡ƒ“‚·‚é
-				m_girl_hitReactionFlag = true;		// —‚Ìq‚ªHIT‚µ‚½‚ÌƒŠƒAƒNƒVƒ‡ƒ“‚ğ‚·‚é
-			}
-			else if (m_target[m_targetCount]->GetPosX() <= -80)
-			{
-				m_girl_ReactionFlag = true;			// —‚Ìq‚ªƒŠƒAƒNƒVƒ‡ƒ“‚·‚é
-				m_girl_missReactionFlag = true;		// —‚Ìq‚ªmiss‚µ‚½‚ÌƒŠƒAƒNƒVƒ‡ƒ“‚ğ‚·‚é
-			}
-		}
-		m_target[m_targetCount]->Reaction(m_hit_ui[m_targetCount], m_iceHitFlagBuffer);
-
-		m_player->Update(_deltaTime);
-
-		m_camera->Update(*m_player);
-
-
-		// UI‚Ì’†‰Ø–º‚ğ“®‚©‚·
-		if (m_girlUpFlag)
-		{
-			m_girl_Y--;
-			if (m_girl_Y < GIRL_MIN_Y)
-			{
-				m_girl_Y = GIRL_MIN_Y;
-				m_girlUpFlag = false;
-			}
-		}
-		else if (!m_girlUpFlag)
-		{
-			m_girl_Y++;
-			if (m_girl_Y > 0)
-			{
-				m_girl_Y = 0;
-				m_girlUpFlag = true;
-			}
-		}
-
-
-
-		if (m_targetCount == enemyNum)
-		{
-			m_finishFlag = TRUE;
-		}
-		if (m_finishFlag == TRUE)
-		{
-			m_fadeOutFlag = true;
-		}
-		if (m_fadeOutFinishFlag)
-		{
-			// scoreUI‚ÌƒXƒRƒA‚ğResult‚Ìscore•Ï”‚ÉƒZƒbƒg
-			return new Result(m_score_ui[m_targetCount]->GetScore());				//	ƒŠƒUƒ‹ƒgƒV[ƒ“‚ÉØ‚è‘Ö‚¦‚é
-		}
-		break;
-	default:
-		break;
-	}
-	return this;						//	ƒQ[ƒ€ƒV[ƒ“‚ğ•\¦‚µ‘±‚¯‚é
-}
-
-
-void TestSceneNakamura::Draw()
-{
-	if (!m_fadeInFinishFlag)
-	{
-		// ƒtƒF[ƒhƒCƒ“ˆ—
-		for (int i = 0; i < 255; i += FADE_IN_SPEED)
-		{
-			// •`‰æ‹P“x‚ğƒZƒbƒg
-			SetDrawBright(i, i, i);
-
-			PlaySoundMem(m_doorSoundHandle, DX_PLAYTYPE_BACK, FALSE);
-			ChangeVolumeSoundMem(m_volumePal + DOOR_VOLUME_PAL, m_doorSoundHandle);
-
-			// ƒOƒ‰ƒtƒBƒbƒN‚ğ•`‰æ
-			DrawGraph(0, 0, m_backGraphHandle, TRUE);
-			DrawGraph(0, m_girl_Y, m_girlGraphHandle, TRUE);
-			DrawGraph(0, m_lady_Y, m_ladyGraphHandle, TRUE);
-			ScreenFlip();
-		}
-		m_fadeInFinishFlag = true;
-	}
-	//	”wŒi
-	DrawGraph(0, 0, m_backGraphHandle, TRUE);
-	DrawGraph(0, m_girl_Y, m_girlGraphHandle, TRUE);
-	//—‚Ìq‚ÌƒŠƒAƒNƒVƒ‡ƒ“•`‰æ
-	if (m_girl_hitReactionFlag == true)				// hit‚µ‚½‚È‚ç‚Î
-	{
-		DrawGraph(300, m_girl_Y + 450, m_girl_hitReaction_GraphHandle, TRUE);
-	}
-	else if (m_girl_missReactionFlag == true)		// miss‚µ‚½‚È‚ç‚Î
-	{
-		DrawGraph(300, m_girl_Y + 450, m_girl_missReaction_GraphHandle, TRUE);
-	}
-	DrawGraph(0, m_lady_Y, m_ladyGraphHandle, TRUE);
-	// –Úˆó‚Æ‚È‚éŠ÷
-	m_mark->Mark_Draw();
-	// ƒ^[ƒQƒbƒg(ƒAƒCƒX)
-	for (int i = 0; i <= m_targetCount; i++)
-	{
-		m_target[i]->Draw();
-	}
-
-	// ƒvƒŒ[ƒ„[
-	m_player->Draw();
-
-	// I—¹
-	if (m_target[enemyNum - 1]->GetIceState() == Target_State::END_SHOT)
-	{
-		DrawGraph(0, 0, m_finishGraphHandle, TRUE);							//	ÅŒã‚ÌƒGƒlƒ~[‚ªËo‚³‚êI‚í‚Á‚½‚ç"ƒQ[ƒ€I—¹"‚Ì•\¦
-	}
-	for (int i = 0; i < enemyNum; ++i)
-	{
-		m_score_ui[i]->Draw();
-	}
-	for (int i = 0; i < enemyNum; ++i)
-	{
-		m_hit_ui[i]->Draw();
-	}
-
-	// ƒGƒtƒFƒNƒg‚ÌÄ¶
-	if (!(m_effect->GetNowPlaying() == 0) && m_target[m_targetCount]->GetHitIce())
-	{
-		m_effect->PlayEffekseer(VGet(0, 20, 0));
-		m_target[m_targetCount]->SetHitIce(false);
-	}
-
-	if (m_state == GAME_SCENE_STATE::COUNTDOWN)
-	{
-		// “§‰ß‚µ‚Ä•`‰æ
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 190);
-		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
-		// “§‰ß‚ğŒ³‚É–ß‚·
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-	}
-	DrawGraph(0, 0, m_manualGraphHandle, TRUE);							//	‘€ìà–¾‚ğ•\¦
-	// ƒJƒEƒ“ƒgƒ_ƒEƒ“‚Ì•`‰æ
-	if (m_state == GAME_SCENE_STATE::COUNTDOWN)
-	{
-		int Count = (COUNTDOWN)-(GetNowCount() / 1000 - m_startTime);
-		DrawExtendFormatString(960, 540, 10.0, 10.0, GetColor(255, 0, 0), "%d", Count);
-	}
-
-	/*m_obstructManager->Draw();*/
-	/*DrawString(0, 0, "ƒQ[ƒ€‰æ–Ê‚Å‚·", GetColor(255, 255, 255));*/
-
-	// ƒtƒF[ƒhƒAƒEƒgˆ—
-	if (m_fadeOutFlag)
-	{
-		for (int i = 0; i < 255; i += FADE_OUT_SPEED)
-		{
-			// •`‰æ‹P“x‚ğƒZƒbƒg
-			SetDrawBright(255 - i, 255 - i, 255 - i);
-
-			// ƒOƒ‰ƒtƒBƒbƒN‚ğ•`‰æ
-			DrawGraph(0, 0, m_backGraphHandle, FALSE);
-			DrawGraph(0, 0, m_finishGraphHandle, TRUE);
-			DrawGraph(0, m_girl_Y, m_girlGraphHandle, TRUE);
-			DrawGraph(0, m_lady_Y, m_ladyGraphHandle, TRUE);
-			ScreenFlip();
-		}
-		m_fadeOutFinishFlag = true;
-
-	}
-}
-
-void TestSceneNakamura::Sound()
-{
-	//	ƒQ[ƒ€I—¹‚ÉŒø‰Ê‰¹‚ğ—¬‚·
-	if (m_target[enemyNum - 1]->GetIceState() == Target_State::END_SHOT)
-	{
-		StopSoundMem(m_soundHandle);
-		PlaySoundMem(m_finishSoundHandle, DX_PLAYTYPE_BACK, FALSE);
-		ChangeVolumeSoundMem(m_volumePal + GONG_VOLUME_PAL, m_finishSoundHandle);
-	}
-	//	ƒQ[ƒ€’†‚ÉBGM‚ğ—¬‚·
-	if (m_finishFlag == FALSE)
-	{
-		PlaySoundMem(m_soundHandle, DX_PLAYTYPE_BACK, FALSE);
-		ChangeVolumeSoundMem(m_volumePal, m_soundHandle);
-	}
-}
-
-void TestSceneNakamura::Load()
-{
-	//	ƒOƒ‰ƒtƒBƒbƒNƒnƒ“ƒhƒ‹‚ÉƒZƒbƒg
-	m_finishGraphHandle = LoadGraph("data/img/gameEnd.png");
-	m_backGraphHandle = LoadGraph("data/img/gameBack.png");
-	m_soundHandle = LoadSoundMem("data/sound/gameBgm.ogg");
-	m_finishSoundHandle = LoadSoundMem("data/sound/gameEnd.wav");
-	m_girlGraphHandle = LoadGraph("data/img/chinaGirl.png");
-	m_girl_missReaction_GraphHandle = LoadGraph("data/img/chinaGirl_aseri(01).png");	//  —‚Ìq‚Ì”½‰‚Ì‰æ‘œƒnƒ“ƒhƒ‹‚ğƒ[ƒh
-	m_girl_hitReaction_GraphHandle = LoadGraph("data/img/chinaGirl_iine.png");			//  —‚Ìq‚Ì”½‰‚Ì‰æ‘œƒnƒ“ƒhƒ‹‚ğƒ[ƒh
-	m_ladyGraphHandle = LoadGraph("data/img/chinaLady.png");
-	m_manualGraphHandle = LoadGraph("data/img/manual.png");
-
-	//	ƒTƒEƒ“ƒhƒnƒ“ƒhƒ‹‚ÉƒZƒbƒg
-	m_iceSoundHandle = LoadSoundMem("data/sound/throwIce.mp3");
-	m_hitSoundHandle = LoadSoundMem("data/sound/hitIce.mp3");
-	m_missSoundHandle = LoadSoundMem("data/sound/missIce.mp3");
-	m_doorSoundHandle = LoadSoundMem("data/sound/door.ogg");
-
-	int scoreHandle = LoadGraph("data/model/score_ui/score(1).png");
-	m_player = new Player;			//	ƒvƒŒƒCƒ„[ƒNƒ‰ƒX‚ÌƒCƒ“ƒXƒ^ƒ“ƒX‚ğ¶¬
-	m_camera = new Camera;			//	ƒJƒƒ‰ƒNƒ‰ƒX‚ÌƒCƒ“ƒXƒ^ƒ“ƒX‚ğ¶¬
-	m_mark = new Mark;				//	ƒ}[ƒNƒNƒ‰ƒX‚ÌƒCƒ“ƒXƒ^ƒ“ƒX‚ğ¶¬
-	for (int i = 0; i < (enemyNum + 1); i++)
-	{
-		m_target[i] = new Target;
-		m_target[i]->SetInterval(TARGET_SHOT_INTERVAL);
-		m_target[i]->SetAccel(targetSpeed);
-		m_target[i]->SetThrowSound(m_iceSoundHandle);
-		m_target[i]->SetHitSound(m_hitSoundHandle);
-		m_target[i]->SetMissSound(m_missSoundHandle);
-		// m_target[i]->SetAccelVec()
-	}
-
-	for (int i = 0; i < 2; ++i)
-	{
-		for (int j = 0; j < 5; ++j)
-		{
-			m_score_ui[j + (i * 5)] = new UI(j, i, scoreHandle);
-			m_hit_ui[j + (i * 5)] = new UI(j, i, -1);
-		}
-	}
-	// UIƒNƒ‰ƒX‚Ìprivateƒƒ“ƒo•Ï”‚É‰æ‘œƒnƒ“ƒhƒ‹‚ğƒ[ƒh
-	m_score_ui[0]->Load();
-
-	m_effect = new PlayEffect("data/effects/FeatherBomb.efk", 5.0f);
-}
-
-void TestSceneNakamura::DebugKey()
-{
-	// Šm”F—p
-	if (CheckHitKey(KEY_INPUT_A))
-	{
-		if (m_hitCount < enemyNum)
-		{
-			m_hitFlag = true;
-			//m_hit_ui[m_hitCount]->ScoreUpdate(m_hit_ui[m_hitCount], m_hitFlag);
-			m_hitCount++;
-		}
-	}
-	if (CheckHitKey(KEY_INPUT_B))
-	{
-		if (m_hitCount < enemyNum)
-		{
-			m_hitFlag = false;
-			//m_hit_ui[m_hitCount]->ScoreUpdate(m_hit_ui[m_hitCount], m_hitFlag);
-			m_hitCount++;
-		}
-	}
-	if (m_iceThrowFlag)
-	{
-		DrawString(1800, 980, "”­Ë", GetColor(255, 255, 255));
-	}
-	else
-	{
-		DrawString(1800, 980, "–¢”­Ë", GetColor(255, 255, 255));
-	}
-	if (CheckHitKey(KEY_INPUT_RETURN))
-	{
-		m_checkKeyFlag = TRUE;
-	}
-	if (CheckHitKey(KEY_INPUT_RETURN) && m_checkKeyFlag == FALSE)	//	ƒGƒ“ƒ^[‚ª‰Ÿ‚³‚ê‚½‚ç
-	{
-		m_finishFlag = TRUE;
-	}
-}
+	//// ç¢ºèªç”¨å¤‰æ•°
+	int m_hitCount;
+	bool m_hitFlag;
+};
