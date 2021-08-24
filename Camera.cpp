baa6e96f252638f3f1d60716c2d68f5315c278da
hikaru koga <factoru02partx@gmail.com>
2021-08-24 14:04:10 +0900

    カメラ調整、プールのモデル表示

--- Camera.cpp
+++ Camera.cpp
@@ -44,7 +44,7 @@ void Camera::Update(const Player& player)
 	// lerp(VECTOR a, VECTOR b, float t)は
 	// answer = a + ((b-a) * t)
 	// 
-	VECTOR aimPos = VAdd(/*player.GetPos()*/VGet(0,0,0), VScale(/*player.GetDir()*/VGet(0,0,-1), 50.0f));
+	VECTOR aimPos = VAdd(/*player.GetPos()*/VGet(0,0,0), VScale(/*player.GetDir()*/VGet(0,0,-1), 0.0f));
 	// カメラの見る位置を少し上にする
 	aimPos = VAdd(aimPos, VGet(0, 0, 0));
 	VECTOR posToAim = VSub(aimPos, pos);
@@ -52,5 +52,5 @@ void Camera::Update(const Player& player)
 	pos = VAdd(pos, scaledPosToAim);
 #endif
 	// カメラに位置を反映.
-	SetCameraPositionAndTarget_UpVecY(VGet(-80,60,-80), /*player.GetPos()*/VGet(20,0,-20));
+	SetCameraPositionAndTarget_UpVecY(VGet(0,300,0), /*player.GetPos()*/VGet(0,0,-10));
 }
